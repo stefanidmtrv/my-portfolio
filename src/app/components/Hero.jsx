7e0c38d57ce25b3d2124385dc0c41ec7051c6e41
stefanidmtrv <dimitrovastefani00@gmail.com>
2025-02-16 21:39:03 +0000

    Fixed hero background color and card title

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <section className="bg-white dark:bg-gray-900">
+    <section>
       <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
         <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
           Hi, I'm Stefani!
