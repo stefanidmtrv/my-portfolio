eaa46edc7d8720a45f6d73bbd1ac614aba714a22
stefanidmtrv <dimitrovastefani00@gmail.com>
2025-02-16 22:47:46 +0000

    Moved Hero and Overview to a Sections folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Hero from "./components/Hero";
-import Overview from "./components/Overview"
+import Hero from "./sections/Hero";
+import Overview from "./sections/Overview"
 export default function Home() {
   return (
     <div>
--- src/app/sections/Overview.jsx
+++ src/app/sections/Overview.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Card from "./Card";
+import Card from "../components/Card";
 import { FcComboChart } from "react-icons/fc";
 import { FaGlobe, FaCog } from "react-icons/fa";
 const Overview = () => {
